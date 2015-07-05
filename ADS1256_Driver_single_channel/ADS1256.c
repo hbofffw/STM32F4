b0eab7b1f7d297b8ee2825b0b9bd6a8f694fac9b
unlandable <unlandable@me.com>
2015-07-05 20:52:02 +0800

    office

--- ADS1256_Driver_single_channel/ADS1256.c
+++ ADS1256_Driver_single_channel/ADS1256.c
@@ -6,43 +6,43 @@
 
 void SPI2_Init(void)
 {
- SPI_InitTypeDef  SPI_InitStructure;
- GPIO_InitTypeDef GPIO_InitStructure;
- /****Initial SPI2******************/
- 
- /* Enable SPI2 and GPIOB clocks */
- RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
- RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI2, ENABLE);
- 
- 
+    SPI_InitTypeDef  SPI_InitStructure;
+    GPIO_InitTypeDef GPIO_InitStructure;
+    /****Initial SPI2******************/
 
- /* Configure SPI2 pins: NSS, SCK, MISO and MOSI */
- GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
- GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
- GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
- GPIO_Init(GPIOB, &GPIO_InitStructure);
- 
- //SPI2 NSS 
+    /* Enable SPI2 and GPIOB clocks */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI2, ENABLE);
+
+
+
+    /* Configure SPI2 pins: NSS, SCK, MISO and MOSI */
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
+
+    //SPI2 NSS 
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
     GPIO_Init(GPIOB, &GPIO_InitStructure);
- 
- GPIO_SetBits(GPIOB, GPIO_Pin_12);
- 
-  /* SPI2 configuration */ 
- SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex; //SPI1设置为两线全双工
- SPI_InitStructure.SPI_Mode = SPI_Mode_Master;                    //设置SPI2为主模式
- SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;                  //SPI发送接收8位帧结构
- SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;                   //串行时钟在不操作时，时钟为低电平
- SPI_InitStructure.SPI_CPHA = SPI_CPHA_2Edge;                 //第一个时钟沿开始采样数据
- SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;                  //NSS信号由软件（使用SSI位）管理
- SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_256; //定义波特率预分频的值:波特率预分频值为8
- SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;       //数据传输从MSB位开始
- SPI_InitStructure.SPI_CRCPolynomial = 7;         //CRC值计算的多项式
- SPI_Init(SPI2, &SPI_InitStructure);
- /* Enable SPI2  */
- SPI_Cmd(SPI2, ENABLE);  
+
+    GPIO_SetBits(GPIOB, GPIO_Pin_12);
+
+    /* SPI2 configuration */ 
+    SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex; //SPI1设置为两线全双工
+    SPI_InitStructure.SPI_Mode = SPI_Mode_Master;                    //设置SPI2为主模式
+    SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;                  //SPI发送接收8位帧结构
+    SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;                   //串行时钟在不操作时，时钟为低电平
+    SPI_InitStructure.SPI_CPHA = SPI_CPHA_2Edge;                 //第一个时钟沿开始采样数据
+    SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;                  //NSS信号由软件（使用SSI位）管理
+    SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_256; //定义波特率预分频的值:波特率预分频值为8
+    SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;       //数据传输从MSB位开始
+    SPI_InitStructure.SPI_CRCPolynomial = 7;         //CRC值计算的多项式
+    SPI_Init(SPI2, &SPI_InitStructure);
+    /* Enable SPI2  */
+    SPI_Cmd(SPI2, ENABLE);  
 }  
 
 //初始化ADS1256 GPIO
@@ -50,23 +50,23 @@ void Init_ADS1256_GPIO(void)
 {
     GPIO_InitTypeDef GPIO_InitStructure;
 
-	RCC_APB2PeriphClockCmd(RCC_ADS1256Reset | RCC_ADS1256DRDY, ENABLE); 
+    RCC_APB2PeriphClockCmd(RCC_ADS1256Reset | RCC_ADS1256DRDY, ENABLE); 
 
-  	GPIO_InitStructure.GPIO_Pin = GPIO_RCC_ADS1256Reset; 
-  	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-  	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  
-  	GPIO_Init(GPIO_RCC_ADS1256Reset_PORT, &GPIO_InitStructure);  
-  	GPIO_ResetBits(GPIO_RCC_ADS1256Reset_PORT, GPIO_RCC_ADS1256Reset );
+    GPIO_InitStructure.GPIO_Pin = GPIO_RCC_ADS1256Reset; 
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+
+    GPIO_Init(GPIO_RCC_ADS1256Reset_PORT, &GPIO_InitStructure);  
+    GPIO_ResetBits(GPIO_RCC_ADS1256Reset_PORT, GPIO_RCC_ADS1256Reset );
 
 
-	GPIO_InitStructure.GPIO_Pin = GPIO_ADS1256DRDY; 
-  	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-  	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  
-  	GPIO_Init(GPIO_ADS1256DRDY_PORT, &GPIO_InitStructure);  
+    GPIO_InitStructure.GPIO_Pin = GPIO_ADS1256DRDY; 
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 
-	SPI2_Init();
+    GPIO_Init(GPIO_ADS1256DRDY_PORT, &GPIO_InitStructure);  
+
+    SPI2_Init();
 }
 
 
@@ -79,14 +79,14 @@ void Init_ADS1256_GPIO(void)
 //-----------------------------------------------------------------//
 unsigned char SPI_WriteByte(unsigned char TxData)
 {
-  unsigned char RxData=0;
+    unsigned char RxData=0;
 
-  while(SPI_I2S_GetFlagStatus(SPI2,SPI_I2S_FLAG_TXE)==RESET); //                                                   
-  SPI_I2S_SendData(SPI2,TxData);
+    while(SPI_I2S_GetFlagStatus(SPI2,SPI_I2S_FLAG_TXE)==RESET); //                                                   
+    SPI_I2S_SendData(SPI2,TxData);
 
-   while(SPI_I2S_GetFlagStatus(SPI2,SPI_I2S_FLAG_RXNE)==RESET);
+    while(SPI_I2S_GetFlagStatus(SPI2,SPI_I2S_FLAG_RXNE)==RESET);
 
-   RxData=SPI_I2S_ReceiveData(SPI2);
+    RxData=SPI_I2S_ReceiveData(SPI2);
 
     return RxData;
 } 
@@ -101,25 +101,25 @@ unsigned char SPI_WriteByte(unsigned char TxData)
 void ADS1256WREG(unsigned char regaddr,unsigned char databyte)
 {
     GPIO_ResetBits(GPIOB, GPIO_Pin_12);
-	while(GPIO_ReadInputDataBit(GPIO_ADS1256DRDY_PORT,GPIO_ADS1256DRDY));//当ADS1256_DRDY为低时才能写寄存器
-	//向寄存器写入数据地址
+    while(GPIO_ReadInputDataBit(GPIO_ADS1256DRDY_PORT,GPIO_ADS1256DRDY));//当ADS1256_DRDY为低时才能写寄存器
+    //向寄存器写入数据地址
     SPI_WriteByte(ADS1256_CMD_WREG | (regaddr & 0x0F));
     //写入数据的个数n-1
     SPI_WriteByte(0x00);
     //向regaddr地址指向的寄存器写入数据databyte
     SPI_WriteByte(databyte);
-	GPIO_SetBits(GPIOB, GPIO_Pin_12);
+    GPIO_SetBits(GPIOB, GPIO_Pin_12);
 }
 
 
 //初始化ADS1256
 void ADS1256_Init(void)
 {
-	ADS1256WREG(ADS1256_STATUS,0x06);               // 高位在前、校准、使用缓冲
-//	ADS1256WREG(ADS1256_MUX,0x08);                  // 初始化端口A0为‘+’，AINCOM位‘-’
-	ADS1256WREG(ADS1256_ADCON,0x00);                // 放大倍数1
-	ADS1256WREG(ADS1256_DRATE,ADS1256_DRATE_10SPS);  // 数据5sps
-	ADS1256WREG(ADS1256_IO,0x00);               
+    ADS1256WREG(ADS1256_STATUS,0x06);               // 高位在前、校准、使用缓冲
+    //	ADS1256WREG(ADS1256_MUX,0x08);                  // 初始化端口A0为‘+’，AINCOM位‘-’
+    ADS1256WREG(ADS1256_ADCON,0x00);                // 放大倍数1
+    ADS1256WREG(ADS1256_DRATE,ADS1256_DRATE_10SPS);  // 数据5sps
+    ADS1256WREG(ADS1256_IO,0x00);               
 }
 
 //读取AD值
@@ -127,20 +127,20 @@ unsigned int ADS1256ReadData()
 {
     unsigned char i=0;
     unsigned int sum=0;
-	unsigned int r=0;
-	GPIO_ResetBits(GPIOB, GPIO_Pin_12);;
+    unsigned int r=0;
+    GPIO_ResetBits(GPIOB, GPIO_Pin_12);;
 
-	while(GPIO_ReadInputDataBit(GPIO_ADS1256DRDY_PORT,GPIO_ADS1256DRDY));               //当ADS1256_DRDY为低时才能写寄存器 
-//	ADS1256WREG(ADS1256_MUX,channel);		//设置通道
-	SPI_WriteByte(ADS1256_CMD_SYNC);
-	SPI_WriteByte(ADS1256_CMD_WAKEUP);	               
-	SPI_WriteByte(ADS1256_CMD_RDATA);
+    while(GPIO_ReadInputDataBit(GPIO_ADS1256DRDY_PORT,GPIO_ADS1256DRDY));               //当ADS1256_DRDY为低时才能写寄存器 
+    //	ADS1256WREG(ADS1256_MUX,channel);		//设置通道
+    SPI_WriteByte(ADS1256_CMD_SYNC);
+    SPI_WriteByte(ADS1256_CMD_WAKEUP);	               
+    SPI_WriteByte(ADS1256_CMD_RDATA);
 
-   	sum |= (SPI_WriteByte(0xff) << 16);
-	sum |= (SPI_WriteByte(0xff) << 8);
-	sum |= SPI_WriteByte(0xff);
+    sum |= (SPI_WriteByte(0xff) << 16);
+    sum |= (SPI_WriteByte(0xff) << 8);
+    sum |= SPI_WriteByte(0xff);
 
-	GPIO_SetBits(GPIOB, GPIO_Pin_12); 
+    GPIO_SetBits(GPIOB, GPIO_Pin_12); 
     return sum;
 }
 
@@ -154,7 +154,7 @@ unsigned int ADS1256ReadData()
 //-----------------------------------------------------------------//
 unsigned int ADS_sum(unsigned char channel)
 {
-	ADS1256WREG(ADS1256_MUX,channel);		//设置通道
-	return ADS1256ReadData();//读取AD值，返回24位数据。
+    ADS1256WREG(ADS1256_MUX,channel);		//设置通道
+    return ADS1256ReadData();//读取AD值，返回24位数据。
 }
 
--- ADS1256_Driver_single_channel/main.c
+++ ADS1256_Driver_single_channel/main.c
@@ -1,22 +1,22 @@
 /**
-  ******************************************************************************
-  * @file    USART/Printf/main.c 
-  * @author  MCD Application Team
-  * @version V3.3.0
-  * @date    04/16/2010
-  * @brief   Main program body
-  ******************************************************************************
-  * @copy
-  *
-  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
-  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
-  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
-  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
-  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
-  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
-  *
-  * <h2><center>&copy; COPYRIGHT 2010 STMicroelectronics</center></h2>
-  */ 
+ ******************************************************************************
+ * @file    USART/Printf/main.c 
+ * @author  MCD Application Team
+ * @version V3.3.0
+ * @date    04/16/2010
+ * @brief   Main program body
+ ******************************************************************************
+ * @copy
+ *
+ * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+ * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+ * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
+ * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+ * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+ * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+ *
+ * <h2><center>&copy; COPYRIGHT 2010 STMicroelectronics</center></h2>
+ */ 
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f10x.h"
@@ -27,12 +27,12 @@
 #include <stdio.h>
 
 /** @addtogroup STM32F10x_StdPeriph_Examples
-  * @{
-  */
+ * @{
+ */
 
 /** @addtogroup USART_Printf
-  * @{
-  */ 
+ * @{
+ */ 
 
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
@@ -56,129 +56,129 @@ unsigned char data[16];
 /* Private function prototypes -----------------------------------------------*/
 
 #ifdef __GNUC__
-  /* With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf
-     set to 'Yes') calls __io_putchar() */
-  #define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+/* With GCC/RAISONANCE, small printf (option LD Linker->Libraries->Small printf
+   set to 'Yes') calls __io_putchar() */
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
 #else
-  #define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
+#define PUTCHAR_PROTOTYPE int fputc(int ch, FILE *f)
 #endif /* __GNUC__ */
-  
-  
+
+
 /* Private functions ---------------------------------------------------------*/
 
 void Init_UART2()
 {
-	USART_InitTypeDef USART_InitStructure;
-	/* USARTx configured as follow:
-        - BaudRate = 115200 baud  
-        - Word Length = 8 Bits
-        - One Stop Bit
-        - No parity
-        - Hardware flow control disabled (RTS and CTS signals)
-        - Receive and transmit enabled
-  */	
-	USART_InitStructure.USART_BaudRate = 115200;
-  	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-  	USART_InitStructure.USART_StopBits = USART_StopBits_1;
-  	USART_InitStructure.USART_Parity = USART_Parity_No;
-  	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-  	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+    USART_InitTypeDef USART_InitStructure;
+    /* USARTx configured as follow:
+       - BaudRate = 115200 baud  
+       - Word Length = 8 Bits
+       - One Stop Bit
+       - No parity
+       - Hardware flow control disabled (RTS and CTS signals)
+       - Receive and transmit enabled
+       */	
+    USART_InitStructure.USART_BaudRate = 115200;
+    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART_InitStructure.USART_StopBits = USART_StopBits_1;
+    USART_InitStructure.USART_Parity = USART_Parity_No;
+    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
 
-  	STM_EVAL_COMInit(COM2, &USART_InitStructure);
+    STM_EVAL_COMInit(COM2, &USART_InitStructure);
 }
 
 
 /**
-  * @brief  Main program
-  * @param  None
-  * @retval None
-  */
+ * @brief  Main program
+ * @param  None
+ * @retval None
+ */
 int main(void)
 {
-	unsigned char i=0;
-	long ulResult;
-	long double ldVolutage;
+    unsigned char i=0;
+    long ulResult;
+    long double ldVolutage;
 
-	Init_UART2();
+    Init_UART2();
 
-	Init_ADS1256_GPIO(); //初始化ADS1256 GPIO管脚 
-	GPIO_SetBits(GPIOC, GPIO_Pin_10 );  
-	Delay(0xFF);
+    Init_ADS1256_GPIO(); //初始化ADS1256 GPIO管脚 
+    GPIO_SetBits(GPIOC, GPIO_Pin_10 );  
+    Delay(0xFF);
 
-	ADS1256_Init();
+    ADS1256_Init();
 
-	while(1)
-	{	
-		for(i = 0;i < 8;i++)
-		{
-			 ulResult = ADS_sum( (i << 4) | ADS1256_MUXN_AINCOM);	
-			//ulResult = ADS_sum( ADS1256_MUXP_AIN0 | ADS1256_MUXN_AINCOM);	
-			if( ulResult & 0x800000 )
-			{
-			 	ulResult = ~(unsigned long)ulResult;
-				ulResult &= 0x7fffff;
-				ulResult += 1;
-				ulResult = -ulResult;
-			}
-		
-			ldVolutage = (long double)ulResult*0.59604644775390625;
+    while(1)
+    {	
+        for(i = 0;i < 8;i++)
+        {
+            ulResult = ADS_sum( (i << 4) | ADS1256_MUXN_AINCOM);	
+            //ulResult = ADS_sum( ADS1256_MUXP_AIN0 | ADS1256_MUXN_AINCOM);	
+            if( ulResult & 0x800000 )
+            {
+                ulResult = ~(unsigned long)ulResult;
+                ulResult &= 0x7fffff;
+                ulResult += 1;
+                ulResult = -ulResult;
+            }
 
-			printf("第%d通道:",(i & 0x07)?(i & 0x07) - 1:7);
-			printf("%lf",ldVolutage); 	//double
-			printf("uV\r\n");
+            ldVolutage = (long double)ulResult*0.59604644775390625;
 
-			//printf("%x",(unsigned long)ulResult);//16
-			Delay(0x3fFFF);
-		}
-	}
+            printf("第%d通道:",(i & 0x07)?(i & 0x07) - 1:7);
+            printf("%lf",ldVolutage); 	//double
+            printf("uV\r\n");
+
+            //printf("%x",(unsigned long)ulResult);//16
+            Delay(0x3fFFF);
+        }
+    }
 }
 
 /**
-  * @brief  Retargets the C library printf function to the USART.
-  * @param  None
-  * @retval None
-  */
+ * @brief  Retargets the C library printf function to the USART.
+ * @param  None
+ * @retval None
+ */
 PUTCHAR_PROTOTYPE
 {
-  /* Place your implementation of fputc here */
-  /* e.g. write a character to the USART */
-  USART_SendData(EVAL_COM2, (uint8_t) ch);
+    /* Place your implementation of fputc here */
+    /* e.g. write a character to the USART */
+    USART_SendData(EVAL_COM2, (uint8_t) ch);
 
-  /* Loop until the end of transmission */
-  while (USART_GetFlagStatus(EVAL_COM2, USART_FLAG_TC) == RESET)
-  {}
+    /* Loop until the end of transmission */
+    while (USART_GetFlagStatus(EVAL_COM2, USART_FLAG_TC) == RESET)
+    {}
 
-  return ch;
+    return ch;
 }
 
 
 #ifdef  USE_FULL_ASSERT
 
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t* file, uint32_t line)
 { 
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+    /* User can add his own implementation to report the file name and line number,
+ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
 
-  /* Infinite loop */
-  while (1)
-  {
-  }
+    /* Infinite loop */
+    while (1)
+    {
+    }
 }
 #endif
 
 /**
-  * @}
-  */ 
+ * @}
+ */ 
 
 /**
-  * @}
-  */ 
+ * @}
+ */ 
 
 /******************* (C) COPYRIGHT 2010 STMicroelectronics *****END OF FILE****/
